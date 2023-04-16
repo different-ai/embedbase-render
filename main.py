7309d0f80969dae265113d768b45a2485ae18351
Louis Beaumont <louis.beaumont@gmail.com>
2023-04-16 10:57:02 +0200

    chore: use supabase instead

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import os
 import uvicorn
 from embedbase import get_app
 
-from embedbase.database.postgres_db import Postgres
+from embedbase.database.supabase_db import Supabase
 from embedbase.embedding.openai import OpenAI
 
  
 app = (
     get_app()
     .use_embedder(OpenAI(os.getenv("OPENAI_API_KEY")))
-    .use_db(Postgres())
+    .use_db(Supabase(os.getenv("SUPABASE_URL"), os.getenv("SUPABASE_KEY")))
 ).run()
 
 if __name__ == "__main__":
