61ea46a4176fcbab5fe66aa343827ae9e60625ed
louis030195 <louis.beaumont@gmail.com>
2023-04-24 17:13:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import os
 import uvicorn
 from embedbase import get_app
 
-from embedbase.database.supabase_db import Supabase
+from embedbase.database.memory_db import MemoryDatabase
 from embedbase.embedding.openai import OpenAI
 
 
 app = (
     get_app()
     .use_embedder(OpenAI(os.getenv("OPENAI_API_KEY")))
-    .use_db(Supabase(os.getenv("SUPABASE_URL"), os.getenv("SUPABASE_KEY")))
+    .use_db(MemoryDatabase())
 ).run()
 
 if __name__ == "__main__":
